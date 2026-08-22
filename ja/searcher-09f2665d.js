0a21675a923a57ced36001570141255960447ace
mosuka <mosuka@users.noreply.github.com>
2026-08-22 12:12:22 +0000

    deploy: 065ca36728467019b69f979e6247d1ead2b71d25

--- ja/searcher-09f2665d.js
+++ ja/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-509a4c21.js',
+                path_to_root + 'searchindex-7eccd0f3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
